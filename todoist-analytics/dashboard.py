32a9ec96829523ae17f5cf9b8bc22ea105646e95
Bruno Rodrigues Silva <b.rosilva1@gmail.com>
2021-03-28 23:10:24 -0300

    dashboard mvp

--- todoist-analytics/dashboard.py
+++ todoist-analytics/dashboard.py
@@ -6,7 +6,8 @@ from datetime import datetime, timedelta
 from credentials import token
 import plotly.express as px
 
-date_to_filter = st.slider('days back', 0, 30, 8)  # min: 0h, max: 23h, default: 17h
+date_to_filter = st.slider('days back', 0, 30, 8) 
+remove_weekends = st.checkbox("Remove Weekends?", False)
 
 dc = data_collector(token)
 df_full = dc.collect()
@@ -15,13 +16,23 @@ df_full['datehour_completed'] = pd.DatetimeIndex(df_full['datehour_completed']).
 
 filtered_df = df_full[df_full['datehour_completed'] >= datetime.today() - timedelta(days=date_to_filter)]
 filtered_df['date_completed'] = pd.to_datetime(filtered_df['datehour_completed']).dt.date
+filtered_df['date_completed_weekday'] = pd.to_datetime(filtered_df['datehour_completed']).dt.day_name()
+
 
 st.title("Todoist Analytics Report")
-
+st.markdown(f"Analyzing data from {filtered_df.date_completed.min()} until {filtered_df.date_completed.max()}")
+st.markdown(f"a grand total of {filtered_df.id.nunique()} completed tasks in {filtered_df.project_id.nunique()} projects")
 
 
 df_all_g = filtered_df[['date_completed', 'project-id', 'id', 'content']].groupby(['date_completed'], as_index=False).nunique()
-
+df_all_g['date_completed'] = df_all_g['date_completed'].astype(str)
 
 fig = px.bar(df_all_g, x="date_completed", y="id", title='Daily completed tasks', hover_name='project-id')
+
+if remove_weekends:
+    fig.update_xaxes(
+        rangebreaks=[
+            { 'pattern': 'day of week', 'bounds': [6, 1]}
+        ]
+    )
 st.plotly_chart(fig)
