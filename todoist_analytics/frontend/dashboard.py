1ed0525e97f787eac5b5e6e684eeff41b8ad4c0e
Bruno Rodrigues <bruno.rsilva@g.globo>
2021-10-08 20:36:23 -0300

    getting user token via terminal

--- todoist_analytics/frontend/dashboard.py
+++ todoist_analytics/frontend/dashboard.py
@@ -12,12 +12,9 @@ def create_app():
     st.title("Todoist Analytics Report")
 
     # date_to_filter = st.slider('days back', 0, 30, 8)
-
+    
     with st.spinner("Getting your data :)"):
-        completed_tasks = get_data(token)
-
-    # completed_tasks = completed_tasks.loc[completed_tasks['datehour_completed'] >= pd.to_datetime(
-    #     'today').tz_localize('America/Sao_Paulo') - timedelta(days=date_to_filter)]
+        completed_tasks = get_data(token)    
 
     completed_tasks = date_filter(completed_tasks, "date range filter")
     completed_tasks = last_week_filter(completed_tasks, "filter current week")
@@ -26,21 +23,6 @@ def create_app():
     completed_tasks = weekend_filter(completed_tasks, "remove weekends")
     completed_tasks = project_filter(completed_tasks, "select the desired project")
 
-    st.markdown(
-        f"Analyzing data since {completed_tasks.completed_date.min()} until {completed_tasks.completed_date.max()}"
-    )
-    st.markdown(
-        f"a grand total of {len(completed_tasks.drop_duplicates())} completed tasks in {completed_tasks.project_id.nunique()} projects"
-    )
-
-    completed_tasks_radio = st.radio("Choose your view", ["total", "per project"])
-    
-    if completed_tasks_radio == "total":
-        st.plotly_chart(completed_tasks_per_day(completed_tasks))
-    else:
-        st.plotly_chart(completed_tasks_per_day_per_project(completed_tasks))
-
-    st.plotly_chart(one_hundred_stacked_bar_plot_per_project(completed_tasks))
     col1, col2, col3, col4 = st.columns(4)
     col1.metric("completed tasks", len(completed_tasks.drop_duplicates()))
     col2.metric("projects", completed_tasks.project_id.nunique())
@@ -57,8 +39,22 @@ def create_app():
     )
     col4.metric(
         "tasks per day", 
-        round(len(completed_tasks.drop_duplicates())/(completed_tasks['completed_date'].max()-completed_tasks['completed_date'].min()).days, 1)
+        round(len(completed_tasks.drop_duplicates())/((completed_tasks['completed_date'].max()-completed_tasks['completed_date'].min()).days), 1)
     )
+
+
+    st.markdown(
+        f"Analyzing data since {completed_tasks.completed_date.min()} until {completed_tasks.completed_date.max()}"
+    )
+
+    completed_tasks_radio = st.radio("Choose your view", ["total", "per project"])
+    
+    if completed_tasks_radio == "total":
+        st.plotly_chart(completed_tasks_per_day(completed_tasks))
+    else:
+        st.plotly_chart(completed_tasks_per_day_per_project(completed_tasks))
+
+    st.plotly_chart(one_hundred_stacked_bar_plot_per_project(completed_tasks))
     
 
     # st.plotly_chart(calendar_plot(completed_tasks))
