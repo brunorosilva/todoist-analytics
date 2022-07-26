b5bdf244d7644f01f5f8752d55ebfebe70cca433
MarianoOG <contacto@marianoog.com>
2022-07-27 01:20:17 +0200

    auth file improved (removed code for old versions and warnings solved

--- Homepage.py
+++ Homepage.py
@@ -3,21 +3,24 @@ from src.auth import run_auth
 from src.data import get_data
 
 
+def render():
+    st.title("Welcome to Todoist Analytics")
+    st.write("This is a simple app to track your habits.")
+    st.info("Your data is loaded, you can start using this app now.")
+
+
 if __name__ == "__main__":
     # Set page config
     st.set_page_config(page_title="Todoist Analytics", layout="wide", page_icon="📊")
 
-    st.title("Welcome to Todoist Analytics")
+    if 'data_loaded' not in st.session_state:
+        token = run_auth()
+        if token is not None:
+            with st.spinner("Getting your data :)"):
+                completed_tasks, active_tasks = get_data(token)
+                st.session_state["completed_tasks"] = completed_tasks
+                st.session_state["active_tasks"] = active_tasks
+                st.session_state["data_loaded"] = True
 
-    token = run_auth()
-    if token is not None:
-        with st.spinner("Getting your data :)"):
-            completed_tasks, active_tasks = get_data(token)
-            st.session_state["completed_tasks"] = completed_tasks
-            st.session_state["active_tasks"] = active_tasks
-            st.session_state["data_loaded"] = True
-
-    # Add message once the data is loaded
     if 'data_loaded' in st.session_state:
-        st.write("This is a simple app to track your habits.")
-        st.info("Your data is loaded, you can start using this app now.")
+        render()
--- src/auth.py
+++ src/auth.py
@@ -5,47 +5,44 @@ from src import session_state
 from src.credentials import client_id, client_secret
 
 
-async def get_auth(client_id, client_secret):
-    authorization_url = f"https://todoist.com/oauth/authorize?" \
-                        f"client_id={client_id}&" \
-                        f"scope=data:read&state={client_secret}"
-    return authorization_url
+# Gets the token from todoist oauth
+async def get_token(code):
+    # Post requests for access token
+    data = {"client_id": client_id,
+            "client_secret": client_secret,
+            "code": code}
+    response = requests.post("https://todoist.com/oauth/access_token", data=data).json()
+
+    # Check if response return an error message and return accordingly
+    if response.get("error") is None:
+        return response.get("access_token")
+    else:
+        return None
 
 
-async def get_token(client_id, client_secret, code):
-    data = {
-        "client_id": client_id,
-        "client_secret": client_secret,
-        "code": code,
-    }
-    token = requests.post("https://todoist.com/oauth/access_token", data=data)
-    token = token.json()["access_token"]
-    return token
-
-
+# Runs the authorization and returns the token or
 def run_auth():
-    auth_url = asyncio.run(get_auth(client_id, client_secret))
+    # Get session token from auth url
+    auth_url = f"https://todoist.com/oauth/authorize?client_id={client_id}&scope=data:read&state={client_secret}"
     session = session_state.get(token=None)
 
-    if session.token is None:
-        try:
-            code = st.experimental_get_query_params()["code"]
-        except:
-            st.write(f"""<h2>Please login using this <a target="_self" href="{auth_url}">url</a></h2>""",
-                     unsafe_allow_html=True)
-        else:
-            try:
-                token = asyncio.run(get_token(client_id, client_secret, code=code))
-            except:
-                st.write(
-                    f"""<h2>
-                    This page was refreshed.
-                    Please allow again: <a target="_self"
-                    href="{auth_url}">url</a></h2>""",
-                    unsafe_allow_html=True,
-                )
-            else:
-                session.token = token
-                return session.token
-    else:
+    # If a token is found return it
+    if session.token:
         return session.token
+
+    # Check if code is present in the query parameters
+    params = st.experimental_get_query_params()
+    code = params.get("code")
+
+    # If no code is found send the user to login using
+    if not code:
+        st.write(f"""<h2>Please login using this <a target="_self" href="{auth_url}">url</a></h2>""",
+                 unsafe_allow_html=True)
+    else:
+        token = asyncio.run(get_token(code=code))
+
+        if token:
+            return token
+
+        st.write(f"""<h2>Page refreshed, please allow again: <a target="_self" href="{auth_url}">url</a></h2>""",
+                 unsafe_allow_html=True)
