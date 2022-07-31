7f340c283b22b277765eedf0640514668cc90778
MarianoOG <contacto@marianoog.com>
2022-07-31 02:35:36 +0200

    files for session and utils separated

--- src/utils.py
+++ src/utils.py
@@ -1,86 +1,7 @@
-import asyncio
-import requests
 import streamlit as st
-from streamlit.scriptrunner import get_script_run_ctx
-from streamlit.server.server import Server
+from src.session import run_auth
 from src.data import DataCollector
-from src.credentials import client_id, client_secret
-
-
-# SessionState class that has all the information
-# Credits to https://github.com/uiucanh/streamlit-google-oauth
-class SessionState(object):
-    def __init__(self, **kwargs):
-        for key, val in kwargs.items():
-            setattr(self, key, val)
-
-
-# Get function to get any value from session state
-# Credits to https://github.com/uiucanh/streamlit-google-oauth
-def get_session_state(**kwargs):
-    ctx = get_script_run_ctx()
-    this_session = None
-
-    session_infos = Server.get_current()._session_info_by_id.values()
-
-    for session_info in session_infos:
-        s = session_info.session
-        if not hasattr(s, "_main_dg") and s._uploaded_file_mgr == ctx.uploaded_file_mgr:
-            this_session = s
-
-    if this_session is None:
-        raise RuntimeError("Oh noes. Couldn't get your Streamlit Session object. "
-                           "Are you doing something fancy with threads?")
-
-    # Got the session object! Now let's attach some state into it.
-    if not hasattr(this_session, "_custom_session_state"):
-        this_session._custom_session_state = SessionState(**kwargs)
-
-    return this_session._custom_session_state
-
-
-# Gets the token from todoist oauth
-async def get_token(code):
-    # Post requests for access token
-    data = {"client_id": client_id,
-            "client_secret": client_secret,
-            "code": code}
-    response = requests.post("https://todoist.com/oauth/access_token", data=data).json()
-
-    # Check if response return an error message and return accordingly
-    if response.get("error") is None:
-        return response.get("access_token")
-    else:
-        return None
-
-
-# Runs the authorization and returns the token or
-def run_auth():
-    # Get session token from auth url
-    auth_url = f"https://todoist.com/oauth/authorize?client_id={client_id}&scope=data:read&state={client_secret}"
-    session = get_session_state(token=None)
-
-    # If a token is found return it
-    if session.token:
-        return session.token
-
-    # Check if code is present in the query parameters
-    params = st.experimental_get_query_params()
-    code = params.get("code")
-
-    if not code:
-        st.write(f"""<h1>Welcome to Todoist analytics</h1>
-                 <h2>Please login using this <a target="_self" href="{auth_url}">url</a></h2>""",
-                 unsafe_allow_html=True)
-    else:
-        token = asyncio.run(get_token(code=code))
-
-        if token:
-            return token
-
-        st.write(f"""<h1>Page refreshed</h1>
-                 <h2>Please allow again: <a target="_self" href="{auth_url}">url</a></h2>""",
-                 unsafe_allow_html=True)
+from PIL import Image
 
 
 @st.cache(show_spinner=False)
@@ -90,6 +11,12 @@ def get_data(token):
 
 
 def is_data_ready():
+    # Set page config
+    st.set_page_config(page_title="Todoist Analytics", layout="wide", page_icon="📊")
+    image = Image.open('img/analytics_logo_300x300.png')
+    st.sidebar.image(image, caption='An app to track your tasks, habits and more.')
+
+    # Check if user is authenticated and load data
     if 'data_loaded' not in st.session_state:
         token = run_auth()
 
@@ -101,7 +28,5 @@ def is_data_ready():
                 st.session_state["data_loaded"] = True
                 st.info("Your data is loaded, you can start using this app now.")
 
-    if 'data_loaded' in st.session_state:
-        return True
-    else:
-        return False
+    # If user is authenticated, return True
+    return 'data_loaded' in st.session_state
