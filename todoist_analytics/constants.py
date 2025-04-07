321ed04bee8e015e99b02b8d6282619d63fa4d5c
Bruno Chicelli <b.rosilva1@gmail.com>
2025-04-07 02:03:37 -0300

    feat: get var from dotenv

--- todoist_analytics/constants.py
+++ todoist_analytics/constants.py
@@ -1,3 +1,11 @@
+import os
+
+import dotenv
+
+dotenv.load_dotenv()
 TODOIST_SYNC_BASE_API_URL = "https://api.todoist.com/sync/v9"
 COMPLETED_URL_ENDPOINT = "completed/get_all"
 SYNC_ENDPOINT = "sync"
+
+TODOIST_CLIENT_ID = os.getenv("DEV_APP_ID")
+TODOIST_CLIENT_SECRET = os.getenv("DEV_APP_SECRET")
