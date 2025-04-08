b7b93bb5df410c3d61f389aadf95737b2aa1e84b
Bruno Chicelli <b.rosilva1@gmail.com>
2025-04-08 14:02:22 -0300

    fix: get secrets using streamlit

--- todoist_analytics/constants.py
+++ todoist_analytics/constants.py
@@ -1,11 +1,8 @@
-import os
+import streamlit as st
 
-import dotenv
-
-dotenv.load_dotenv()
 TODOIST_SYNC_BASE_API_URL = "https://api.todoist.com/sync/v9"
 COMPLETED_URL_ENDPOINT = "completed/get_all"
 SYNC_ENDPOINT = "sync"
 
-TODOIST_CLIENT_ID = os.getenv("DEV_APP_ID")
-TODOIST_CLIENT_SECRET = os.getenv("DEV_APP_SECRET")
+TODOIST_CLIENT_ID = st.secrets.app_client_id
+TODOIST_CLIENT_SECRET = st.secrets.app_secret
